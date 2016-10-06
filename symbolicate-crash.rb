ed58b2a57109ef6cafb8b66ab604401e64885ea7
Evgeny Khomyakov <ehomyakov@gmail.com>
2016-10-06 15:25:05 +0300

    updated sha for symbolicate-crash

--- symbolicate-crash.rb
+++ symbolicate-crash.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class SymbolicateCrash < ScriptFileFormula
   url 'https://raw.githubusercontent.com/hamstergene/symbolicate-crash/v0.6.1/symbolicate-crash'
-  sha256 'ee0255522d1f01545981aea7ac36925e81bbf215'
+  sha256 '84d766858e8a75acacf2255672848b5007c51bc0cd4a739a11349f63421308ac'
   version '0.6.1'
   homepage 'https://github.com/hamstergene/symbolicate-crash'
 end
