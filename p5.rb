a6dc979c3693e04160103dc284545bce7a0730d8
Evgeny Khomyakov <ehomyakov@gmail.com>
2016-10-06 15:39:06 +0300

    stop using deprecated homebrew APIs

--- p5.rb
+++ p5.rb
@@ -1,12 +1,18 @@
 require "formula"
 
-class P5 < ScriptFileFormula
+class P5 < Formula
   url 'https://raw.githubusercontent.com/hamstergene/p5/v0.5/p5'
-  sha256 '406accd6f84cfbce582cd6a1e38034ab731b8a49'
+  sha256 'bf29d9541e28b6a9e6d72eca07e50e4565aebd62bbc8be30ad8aa07b1e375fc6'
   version '0.5'
   homepage 'https://github.com/hamstergene/p5'
 
   depends_on "python3"
-  depends_on "homebrew/binary/perforce"
+  #  depends_on "homebrew/binary/perforce"
+
+  bottle :unneeded
+
+  def install
+    bin.install "p5"
+  end
 end
 
--- symbolicate-crash.rb
+++ symbolicate-crash.rb
@@ -1,9 +1,15 @@
 require 'formula'
 
-class SymbolicateCrash < ScriptFileFormula
+class SymbolicateCrash < Formula
   url 'https://raw.githubusercontent.com/hamstergene/symbolicate-crash/v0.6.1/symbolicate-crash'
   sha256 '84d766858e8a75acacf2255672848b5007c51bc0cd4a739a11349f63421308ac'
   version '0.6.1'
   homepage 'https://github.com/hamstergene/symbolicate-crash'
+
+  bottle :unneeded
+
+  def install
+    bin.install "symbolicate-crash"
+  end
 end
 
