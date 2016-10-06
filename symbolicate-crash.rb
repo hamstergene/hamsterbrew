41aa54da97024f4535e1f658dd57395fc79235a1
Evgeny Khomyakov <ehomyakov@gmail.com>
2016-10-06 15:13:40 +0300

    moving formulas for my projects into a separate tap

---
+++ symbolicate-crash.rb
@@ -0,0 +1,9 @@
+require 'formula'
+
+class SymbolicateCrash < ScriptFileFormula
+  url 'https://raw.githubusercontent.com/hamstergene/symbolicate-crash/v0.6.1/symbolicate-crash'
+  sha256 'ee0255522d1f01545981aea7ac36925e81bbf215'
+  version '0.6.1'
+  homepage 'https://github.com/hamstergene/symbolicate-crash'
+end
+
