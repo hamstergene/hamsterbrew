8d7f9d321d68d24fd866d14140ea4cc255addd53
Evgeny Khomyakov <ehomyakov@gmail.com>
2016-12-07 14:43:57 +0300

    p5 v0.7

--- p5.rb
+++ p5.rb
@@ -1,13 +1,12 @@
 require "formula"
 
 class P5 < Formula
-  url 'https://raw.githubusercontent.com/hamstergene/p5/v0.5/p5'
-  sha256 'bf29d9541e28b6a9e6d72eca07e50e4565aebd62bbc8be30ad8aa07b1e375fc6'
-  version '0.5'
+  url 'https://raw.githubusercontent.com/hamstergene/p5/v0.7/p5'
+  sha256 '2988ef6a1c2c1432e6b2aac01e2a3403d827a018'
+  version '0.7'
   homepage 'https://github.com/hamstergene/p5'
 
   depends_on "python3"
-  #  depends_on "homebrew/binary/perforce"
 
   bottle :unneeded
 
