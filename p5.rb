b5ea98e4cda6a899c74ed9e04fa5ab4774cd24b3
Evgeny Khomyakov <ehomyakov@gmail.com>
2016-12-07 14:45:15 +0300

    should have used sha256

--- p5.rb
+++ p5.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class P5 < Formula
   url 'https://raw.githubusercontent.com/hamstergene/p5/v0.7/p5'
-  sha256 '2988ef6a1c2c1432e6b2aac01e2a3403d827a018'
+  sha256 '4d5ab54afea7c09d219b77dbd78af3d36b9c9bf671da2c6c57d84bac21153707'
   version '0.7'
   homepage 'https://github.com/hamstergene/p5'
 
