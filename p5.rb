a55c94540303ad1d33039a77c2510c6149b94996
Evgeny Khomyakov <ehomyakov@gmail.com>
2017-06-28 13:36:18 +0300

    publish p5 0.8

--- p5.rb
+++ p5.rb
@@ -1,9 +1,9 @@
 require "formula"
 
 class P5 < Formula
-    url 'https://raw.githubusercontent.com/hamstergene/p5/v0.7.1/p5'
-  sha256 '891954d9e372aadbbab12892915286f3412872521f0190f9081d5423f0be254c'
-  version '0.7.1'
+    url 'https://raw.githubusercontent.com/hamstergene/p5/v0.8/p5'
+  sha256 'f3c87969b65c4689f22b47f0a1273120f511ccd8fba64442734ff11340a42c12'
+  version '0.8'
   homepage 'https://github.com/hamstergene/p5'
 
   depends_on "python3"
