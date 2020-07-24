2121e9bc8185121ee1fdbe81966e9f23cbaee452
Eugene Homyakov <ehomyakov@gmail.com>
2020-07-23 23:34:22 -0700

    symbolicate-crash 0.7.1

--- symbolicate-crash.rb
+++ symbolicate-crash.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class SymbolicateCrash < Formula
-  url 'https://github.com/hamstergene/symbolicate-crash.git', :tag => 'v0.7'
-  sha256 'c3e8d06bee2cf241b1bd732661b86a076bc8795633887a2e8f1a3cc8bcae05ad'
-  version '0.7'
+  url 'https://github.com/hamstergene/symbolicate-crash.git', :tag => 'v0.7.1'
+  sha256 'd7036856c2f9a4a81fb880e0cb58a45c010ef3e57690423b4d05d90132951494'
+  version '0.7.1'
   homepage 'https://github.com/hamstergene/symbolicate-crash'
 
   bottle :unneeded
